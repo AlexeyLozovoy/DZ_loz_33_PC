77cfc0c48392209541c0e245fda923035aa70f8d
AlexeyLozovoy <gvozdik@ua.fm>
2023-02-18 16:31:18 +0200

    new12

--- main.py
+++ main.py
@@ -27,7 +27,6 @@ def send_welcome(message):
     bot.reply_to(message, "люблю!")
 
 
-
 @bot.message_handler(func=lambda message: message.text == "Key")
 def echo_message(message):
     bot.reply_to(message, message.text)
